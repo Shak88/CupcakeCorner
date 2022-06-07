db82c4b2ca5b4aad608507bde8e3a0337d61e55f
Shokri Alnajjar <shokre4@hotmail.com>
2022-06-07 18:35:05 +0400

    small changes

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -34,12 +34,15 @@ struct ContentView: View {
     
     func loadData() async {
         guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")else {
-            print("Invalid data")
+            print("Invalid URL")
             return
         }
         
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
         } catch {
             print("Invalid data")
         }

8cd4265627bef77701345cb796e11353db2dcb51
Shokri Alnajjar <shokre4@hotmail.com>
2022-06-07 18:18:27 +0400

    initial Commit

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,10 +7,42 @@
 
 import SwiftUI
 
+struct Response : Codable  {
+    var results : [Result]
+}
+
+struct Result : Codable {
+    var trackId : Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id:\.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")else {
+            print("Invalid data")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
